124bf5d79a517bd78d6410e7056d731ae2648ab5
taisei <taisei25864@gmail.com>
2024-10-27 12:38:19 +0900

    add gemini

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -4,8 +4,12 @@ import 'src/input_before.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'dart:developer';
+import 'dart:io';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
@@ -22,6 +26,7 @@ class MyApp extends StatelessWidget {
       throw Exception('データの読み込みに失敗しました');
     }
   }
+
 
   Future<Map<String, dynamic>> fetchResult2() async {
     final url = Uri.parse('http://10.0.2.2:8000/result_sleep');
@@ -98,6 +103,37 @@ class DashboardScreen extends StatelessWidget {
 
   const DashboardScreen({Key? key, required this.data, required this.data2, required this.data3}) : super(key: key);
 
+  // data3 を JSON 形式の文字列に変換
+  String getJsonString() {
+    return jsonEncode(data2); // data3 を JSON 形式の文字列に変換
+  }
+  // 生成メソッドを追加
+  Stream<String> generateExampleSentences(String post) {
+    final prompt = '''
+    ---
+    $post
+    ---
+
+    上記の文字列を
+    keyを睡眠時間、valueを集中度として、
+    健康の観点から分析して
+    ''';
+    // final prompt = '''
+    //   1から10まで出力するコードをpythonで書いて
+    // ''';
+
+    final model = GenerativeModel(
+      model: 'gemini-pro',
+      apiKey: dotenv.env['GEMINI_API_KEY']!, // .envからAPIキーを読み込み
+    );
+
+    final content = [Content.text(prompt)];
+    return model.generateContentStream(content).map((response) {
+      return response.text ?? 'No response because Gemini API went wrong.';
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -110,10 +146,12 @@ class DashboardScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(height: 20),
+            SizedBox(height: 30),
             _buildDrinkScoreChart(),
             _buildConsumptionChart(),
             _buildSleepFocusChart(),
+            SizedBox(height: 50),
+            _buildGeneratedExamples(),
           ],          
         ),
       ),
@@ -204,6 +242,51 @@ class DashboardScreen extends StatelessWidget {
       ),
     );
   }
+
+  Widget _buildGeneratedExamples() {
+
+    Future<String> chatting(String inputText) async {
+    var apiKey = dotenv.get('GEMINI_API_KEY');
+    if (apiKey == null) {
+        log('API Key取得失敗');
+        exit(1);
+    }
+    
+    final genModel = GenerativeModel(model: 'gemini-pro', apiKey: apiKey);
+    final content = [Content.text(inputText)];
+
+    final response = await genModel.generateContent(content);
+    String resText = response.text ?? 'Gemini返答失敗';
+    return resText;
+    }
+
+    final jsonString = getJsonString();
+    return Container(
+      height: 800,
+      margin: const EdgeInsets.symmetric(vertical: 30),
+      padding: const EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25),
+        color: const Color.fromARGB(136, 131, 130, 130),
+      ),
+      child: FutureBuilder<String>(
+        future: chatting('このデータを元に健康の観点から分析してみてください。keyは睡眠時間、valueは集中度となっています。: $jsonString '), // chattingメソッドを呼び出す
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('エラー: ${snapshot.error}'));
+          } else {
+            return Text(
+              snapshot.data ?? 'No examples generated',
+              style: const TextStyle(color: Colors.white),
+            );
+          }
+        },
+      ),
+    );
+  }
+  
 
   List<DrinkData> getDrinkData() {
     List<DrinkData> drinkDataList = [];
